012f35577377fadf0a540e7c90e7fa0c536c60fe
Alberto Bellin <54807452+alberto-bellin@users.noreply.github.com>
2021-06-21 17:41:20 +0200

    Add files via upload

---
+++ src/SEED_FROM_URANDOM.f90
@@ -0,0 +1,22 @@
+MODULE MOD_SEED_FROM_URANDOM
+ CONTAINS
+ !#######################################################
+ !#######################################################
+ !#######################################################
+ SUBROUTINE SEED_FROM_URANDOM()
+ IMPLICIT NONE
+ INTEGER :: N
+ INTEGER, DIMENSION(:),ALLOCATABLE :: I
+ CALL RANDOM_SEED ( ) 
+ CALL RANDOM_SEED(SIZE=N)
+ !ALLOCATE(I(N))
+ !OPEN(89, FILE='/dev/urandom', ACCESS='STREAM', FORM='UNFORMATTED')
+ !READ(89) I
+ !CLOSE(89)
+ !PRINT *, "SEEDING RNG AS", I
+ !CALL RANDOM_SEED(PUT=I)
+ ENDSUBROUTINE
+ !#######################################################
+ !#######################################################
+ !#######################################################
+ENDMODULE
